9614d45e89f089b207857505138106ee77aba723
atsushi watanabe <watanabe@sunrisedigital.jp>
2016-12-23 18:19:10 +0900

    I finished up to 'Lifting State Up'.

--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,40 @@ import React from 'react';
 import {render} from 'react-dom';
 
 class Square extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value:null,
+    };
+  }
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.state.value}
       </button>
     );
   }　
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  constructor () {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
+
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+    console.log(squares);
+  }
+
   render() {
     const status = 'Next player: X';
     return (
