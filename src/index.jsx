65dd483a2a4bb55a5933d1d891f2ae79df42fd2d
atsushi watanabe <watanabe@sunrisedigital.jp>
2017-01-31 11:26:51 +0900

    仕切り直し

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Provider} from 'react-dom';
+import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 
-console.log(createStore);
 import App from './containers.js'
 import reducer from './reducers.js'
 
 
 const store = createStore(reducer)
-const element = document.getElementById('root');
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  element
+  document.getElementById('root');
 );
