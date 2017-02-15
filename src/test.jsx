175d16a04a076f4ed803d47fedafca35500af854
atsushi watanabe <watanabe@sunrisedigital.jp>
2017-02-15 19:00:07 +0900

    コンポーネントテスト

--- src/test.jsx
+++ src/test.jsx
@@ -3,42 +3,28 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider, connect } from 'react-redux';
 
-const items = [
-  { name: 'aaa', color: 'black' },
-  { name: 'bbb', color: 'green' },
-  { name: 'ccc', color: 'pink' },
-  { name: 'ddd', color: 'yellow' },
-];
-
-let nameData = '';
-const setNameData = (event) => {
-  nameData = event.target.value;
-  render();
-};
-const addData = () => {
-  items.push({ name: nameData, color: 'black' });
-  nameData = '';
-  render();
-};
-const MyForm = () => (
-  <div>
-    <input type="text" value={nameData} onChange={setNameData} />
-    <button onClick={addData}>Add Data</button>
-  </div>
+const Hello = () => (
+  <div>Hello</div>
 );
 
-const Hello = ({ name, color }) => (
+const Hoge = () => (
   <div>
-    <p style={{ color }}>Hello {name}!</p>
+    <p>HogeHoge</p>
+    <ul>
+      <li>aaaa</li>
+      <li>aaaa</li>
+      <li>aaaa</li>
+      <li>aaaa</li>
+      <li>aaaa</li>
+      <li>aaaa</li>
+    </ul>
   </div>
 );
 
 const App = () => (
   <div>
-    <MyForm />
-    {items.map((item) => (
-      <Hello name={item.name} color={item.color} />
-    ))}
+    <Hello />
+    <Hoge />
   </div>
 );
 
